c36bd77d5a249c5d7e55990f9356ff0ffe360b02
ethge <ethgen@yandex.ru>
2025-09-19 00:25:06 +0300

    test4

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+# main.py
+def add(a, b):
+    return a + b
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    print("Все тесты пройдены!")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hihi2, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    test_add()
